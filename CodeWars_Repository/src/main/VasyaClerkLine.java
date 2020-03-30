cacc789e06ef123de3623afc7c8ae97a648df34b
Haris Siddique <54950181+hsiddique01@users.noreply.github.com>
2020-03-30 18:57:50 +0100

    commit 3
    Test with multiple inputs; 25, 25, and 50

--- CodeWars_Repository/src/main/VasyaClerkLine.java
+++ CodeWars_Repository/src/main/VasyaClerkLine.java
@@ -4,15 +4,34 @@ public class VasyaClerkLine {
 
 	public static String Tickets(int[] input) {
 		int count25 = 0;
+		int count50 = 0;
+		if(input == null || input.length == 0) {
+			return "No";
+		}
 		for(int i: input) {
 			if(i == 25) {
-				count25 ++;
+				count25++;
+			}
+			if(i==50) {
+				count25--;
+				count50++;
+			}
+			if(i==100) {
+				if(count50 > 0 && count25 > 0) {
+					count50--;
+					count25--;
+				}else {
+					count25--;
+					count25--;
+					count25--;
+				}
+			}
+			
+			if(count25 < 0 || count50 < 0) {
+				return "No";
 			}
 		}
-		if(count25 > 0) {
-			return "Yes";
-		}
-		return "No";
+		return "Yes";
 	}
 
 }
--- CodeWars_Repository/src/test/VasyaClerkLineTest.java
+++ CodeWars_Repository/src/test/VasyaClerkLineTest.java
@@ -23,4 +23,25 @@ public class VasyaClerkLineTest {
 	    assertEquals("Response did no match","Yes",result);
 	}
 	
+	@Test 
+	public void TestNoWhen50IsSent() {
+		int[] input = {50};
+		String result = VasyaClerkLine.Tickets(input);
+	    assertEquals("Response did no match","No",result);
+	}
+	
+	@Test
+	public void TestNoWhen100IsSent() {
+		int[] input = {100};
+		String result = VasyaClerkLine.Tickets(input);
+	    assertEquals("Response did no match","No",result);
+	}
+	
+	@Test
+	public void TestYesWhen252550IsSent() {
+		int[] input = {25,25,50};
+		String result = VasyaClerkLine.Tickets(input);
+	    assertEquals("Response did no match","Yes",result);
+	}
+	
 }
